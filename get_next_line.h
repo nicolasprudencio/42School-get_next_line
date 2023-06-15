39ba59282e42b11cab772cb841d43bd406bd0677
nicolasprudencio <nicolas-prudencio@hotmail.com>
2023-06-15 19:43:31 -0300

    cleaning and setting BUFFER_SIZE macro to 42

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 13:33:34 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/06/03 17:39:48 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/06/15 19:06:33 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 11
+#  define BUFFER_SIZE 42
 # endif
 
 char	*get_next_line(int fd);
 int		ft_strlen(const char	*s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
